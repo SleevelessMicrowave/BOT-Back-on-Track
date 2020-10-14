6d2c4dc3a12847acd29deaa4f4a51c52f5b59ed0
emluo <emluo@ctemc.org>
2020-10-13 23:19:12 -0400

    final login

--- lib/Screens/Login/components/background.dart
+++ lib/Screens/Login/components/background.dart
@@ -24,6 +24,18 @@ class Background extends StatelessWidget {
             child: Image.asset(
                 "assets/images/clear.png", height: size.height * 0.45),
           ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/top2.png", height: size.height * 0.25),
+
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset("assets/images/bottom2.png", height: size.height*0.3),
+          ),
           child,
         ],
       ),
